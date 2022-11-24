5a48b6c7079d07e33f5271c7d3a0879cef465236
nkwtnb <n.watanabe0305@gmail.com>
2022-11-24 23:39:27 +0900

    Docker化

---
+++ src/go.mod
@@ -0,0 +1,16 @@
+module main
+
+go 1.19
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang-migrate/migrate/v4 v4.15.2 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	gorm.io/driver/mysql v1.4.4 // indirect
+	gorm.io/gorm v1.24.2 // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("設定ファイルの読み込みに失敗しました: %v", err)
+		os.Exit(1)
+	}
+	fmt.Println("hello from docker")
+	connect()
+}
+
+
+func connect() {
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	fmt.Println(user, password)
+	// dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, dbName)
+	//  dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	fmt.Println(db, err)
+}
