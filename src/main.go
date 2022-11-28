3b019f502c6905509ee221dd9ee5942062aec251
nkwtnb <n.watanabe0305@gmail.com>
2022-11-28 23:52:23 +0900

    マイグレーションファイル数と実際のVersionとの比較

--- src/main.go
+++ src/main.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"github.com/pressly/goose/v3"
 )
 
 func main() {
@@ -27,10 +27,22 @@ func connect() {
 	dbName := os.Getenv("DB_NAME")
 
 	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	fmt.Println(user, password)
+	fmt.Println(user, password,host,dbName)
 	// dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, dbName)
-	//  dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	fmt.Println(db, err)
+	// dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, dbName)
+	dsn := "go_user:go_user@tcp(mysql:3306)/go_tutorial?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// fmt.Println(db, err)
+
+	database, _ := goose.OpenDBWithDriver("mysql", dsn)
+	// fmt.Println(err)
+	// v, _ := goose.EnsureDBVersion(database)
+	m, e := goose.CollectMigrations("db/migrations", -1, goose.MaxVersion)
+	fmt.Println(e)
+	fmt.Println(m)
+	// 全マイグレーション数
+	fmt.Println(m.Len())
+	// 現在のVersion
+	v, _ := goose.GetDBVersion(database)
+	fmt.Println(v)
 }
